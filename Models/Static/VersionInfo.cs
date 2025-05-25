c4a6e52e34ca6d50963a0f9d3e3376a659b3e715
Danijel-James Wynyard <danijel@repasscloud.com>
2025-05-25 18:25:56 +1000

    v2.3.5

--- Models/Static/VersionInfo.cs
+++ Models/Static/VersionInfo.cs
@@ -2,7 +2,7 @@ namespace Ava.Shared.Models.Static;
 
 public static class VersionInfo
 {
-    public static string ClientVersion { get; } = "2.1.4";
+    public static string ClientVersion { get; } = "2.3.5";
     public static string ApiVersion { get; } = "";
     public static string WebAppVersion { get; } = "";
 }
