33eec68a0b96ad98ef0bf0048ebfb3e83d481905
Danijel-James Wynyard <danijel@repasscloud.com>
2025-07-08 22:07:58 +1000

    update dev branch

---
+++ Migrations/20250708120754_updateTravelPolicyDesign.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ava.Shared.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateTravelPolicyDesign : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MaxResults",
+                table: "TravelPolicies",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MaxResults",
+                table: "TravelPolicies");
+        }
+    }
+}
