c430b97befbefed020e450be617a0ca38c773a5d
Danijel-James Wynyard <danijel@repasscloud.com>
2025-07-08 21:59:48 +1000

    update travel policy dto context

--- Models/DTOs/TravelPolicyBookingContextDTO.cs
+++ Models/DTOs/TravelPolicyBookingContextDTO.cs
@@ -1,18 +1,29 @@
 namespace Ava.Shared.Models.DTOs;
-
 public class TravelPolicyBookingContextDTO
 {
-    public string? Id { get; set; }
-    public string? PolicyName { get; set; }
-    public int? AvaClientId { get; set; }
-    public string? AvaClientName { get; set; }
-    public string? Currency { get; set; }
+    [MaxLength(14)]
+    public required string Id { get; set; }
+    public required string PolicyName { get; set; }
+    public int AvaClientId { get; set; }
+    public string AvaClientName { get; set; } = string.Empty;
+
+    [Required]
+    [RegularExpression(@"^[A-Z]{3}$", ErrorMessage = "Currency must be exactly 3 uppercase letters.")]
+    [JsonPropertyName("currency")]
+    [DefaultValue("AUD")]
+    public required string Currency { get; set; } = "AUD";
     public int? MaxFlightPrice { get; set; }
-    public string? DefaultFlightSeating { get; set; }
-    public string? MaxFlightSeating { get; set; }
-    public string? CabinClassCoverage { get; set; }
+    public required string DefaultFlightSeating { get; set; } = "ECONOMY";
+    public required string MaxFlightSeating { get; set; } = "ECONOMY";
+    public required string CabinClassCoverage { get; set; } = "MOST_SEGMENTS";
     public bool NonStopFlight { get; set; } = false;
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    [RegularExpression(@"^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$", ErrorMessage = "Time must be in the format hh:mm:ss.")]
     public string? FlightBookingTimeAvailableFrom { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    [RegularExpression(@"^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$", ErrorMessage = "Time must be in the format hh:mm:ss.")]
     public string? FlightBookingTimeAvailableTo { get; set; }
     public bool EnableSaturdayFlightBookings { get; set; } = false;
     public bool EnableSundayFlightBookings { get; set; } = false;
@@ -23,5 +34,8 @@ public class TravelPolicyBookingContextDTO
     public List<string>? Continents { get; set; }
     public List<string>? Countries { get; set; }
     public List<string>? DisabledCountries { get; set; }
+
+    // [meta]
+    public int MaxResults { get; set; } = 20;
 }
 
