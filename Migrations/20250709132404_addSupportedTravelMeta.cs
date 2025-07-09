cc53cdcc1dcd520bf2c7d4eea2e88bfa614415a4
Danijel-James Wynyard <danijel@repasscloud.com>
2025-07-09 23:24:08 +1000

    update dev branch

---
+++ Migrations/20250709132404_addSupportedTravelMeta.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Ava.Shared.Migrations
+{
+    /// <inheritdoc />
+    public partial class addSupportedTravelMeta : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SupportedTravelContinents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Continent = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SupportedTravelContinents", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SupportedTravelCountries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Country = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SupportedTravelCountries", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SupportedTravelRegions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Region = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SupportedTravelRegions", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SupportedTravelContinents");
+
+            migrationBuilder.DropTable(
+                name: "SupportedTravelCountries");
+
+            migrationBuilder.DropTable(
+                name: "SupportedTravelRegions");
+        }
+    }
+}
